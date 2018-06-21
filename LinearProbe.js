221f8e3cd84afcde767109da804fdc6ac8236e07
Ricky Tsao <Ricky_Tsao@emap.com>
2018-06-21 09:44:45 +0800

    encapsulation changes

--- LinearProbe.js
+++ LinearProbe.js
@@ -19,27 +19,21 @@ var LOAD_FACTOR = 0.5;
 var GROWTH_RATE = 2;
 
 class LinearProbe {
+
   constructor() {
       this.array = new Array(MAX);
-      let _self = this;
+      let _self = this; // for use with private functions
       let _grow = MAX;
 
+      // private
       function hash(key) {
         if (typeof key !== "string") {console.log(`hash - input must be string!`); return null;}
         let value = 0;
         for (let i = 0; i < key.length; i++) { value += key[i].charCodeAt(0); }
         return (value * key.length) % _grow;
-      } // function
+      }
 
-      var sa_probeAndInsert = function probeAndInsert(kvPair, startingIndex, lastIndex) {
-        if (startingIndex >= lastIndex) {return -1;}
-        for (let i = startingIndex; i < lastIndex; i++) {
-            if (!this.array[i]) { this.array[i] = kvPair; return i; }
-        }
-        console.log(`probe - uh oh, cannot find location for ${kvPair}`);
-        return -1;
-      } // function
-
+      // private
       function loadFactor() {
           let count = 0.00;
           for (let i = 0; i < _self.array.length; i++) {
@@ -49,37 +43,51 @@ class LinearProbe {
           return result.toFixed(2);
       }
 
-      this.growArray = function() {
-        console.log("load factor 50% reached, let's double the array! ----- √")
-        let oldArray = this.array;
-        _grow = _grow * GROWTH_RATE;
-        this.array = new Array (_grow);
-        for (let i = 0; i < oldArray.length; i++) {
-          let entry = oldArray[i];
-          if (entry) { this.insert(entry); }
+      // private
+      function probeAndInsert(kvPair, startingIndex, lastIndex) {
+        if (startingIndex >= lastIndex) {return -1;}
+        for (let i = startingIndex; i < lastIndex; i++) {
+            if (!_self.array[i]) { _self.array[i] = kvPair; return i; }
         }
+        console.log(`probe - uh oh, cannot find location for ${kvPair}`);
+        return -1;
       }
 
-      this.insert = function(kvPair) {
+      // private
+      function insert(kvPair) {
         if (kvPair.constructor.name === "KVPair") {
           let index = hash(kvPair.key);
           kvPair.hash = index;
-          if (!this.array[index]) {this.array[index] = kvPair;}
+          if (!_self.array[index]) {_self.array[index] = kvPair;}
           else {
-            let indexInserted = sa_probeAndInsert.call(this, kvPair, index, this.array.length);
+            let indexInserted = probeAndInsert(kvPair, index, _self.array.length);
             if (indexInserted !== -1) console.log(`insert - collision happened at ${index}... probed and inserted (${kvPair.key}) at ${indexInserted}`);
             else {console.log(`insert - !!! can't find a valid spot for ${kvPair.key}:${kvPair.value}`);return -1;}
           }
         } // if KVPair
       }
 
+      // private
+      function growArray() {
+        console.log("load factor 50% reached, let's double the array! ----- √")
+        let oldArray = _self.array;
+        _grow = _grow * GROWTH_RATE;
+        _self.array = new Array (_grow);
+        for (let i = 0; i < oldArray.length; i++) {
+          let entry = oldArray[i];
+          if (entry) { insert(entry); }
+        }
+      }
+
+      // public
       this.insertAndCheckLoad = function(kvPair) {
-        if(this.insert(kvPair)!==-1) {
-          if (loadFactor() > LOAD_FACTOR) {this.growArray();}
-          else {console.log(`insertAndCheckLoad - balance load is fine`);}
+        if(insert(kvPair)!==-1) {
+          if (loadFactor() > LOAD_FACTOR) {growArray();}
+          else {console.log(`√ ${kvPair.key}, ${kvPair.value} inserted`);}
         } else {console.log(`Error: ${kvPair} not inserted`);}
       }
 
+      // public
       this.retrieveIndex = function(key) {
         let index = hash(key);
         for (let i = index; i < this.array.length; i++) {
@@ -89,39 +97,44 @@ class LinearProbe {
         }
       }
 
-      this.delete = function(toDeleteKey) {
-        let toDeleteIndex = this.retrieveIndex(toDeleteKey);
-        if (!this.array[toDeleteIndex]) {console.log(`Sorry! this element does not exist`);return false;}
-        let toDeleteHash = this.array[toDeleteIndex].hash;
-        if (this.array[toDeleteIndex]) {
-          this.array[toDeleteIndex] = null;
-          for (let i = toDeleteIndex+1; i < this.array.length; i++ ) {
-            if (!this.array[i]) { console.log(`cell ${i} is empty. We're done`);return true;}
-            else {
-              if (this.array[i].hash <= toDeleteHash) {
-                  this.array[toDeleteIndex] = this.array[i];
-                  toDeleteIndex = i;
-                  toDeleteHash = this.array[i].hash;
-                  this.array[i] = null;
-              }
-            }
-          } // for
-          console.log(`Sorry! this element does not exist`);return false;
-        }
-        else {console.log(`Sorry! this element does not exist`);return false;}
-      }
-
-      this.print = function() {
-        console.log(`-------- length of array: ${this.array.length} --------`);
-        for (let i = 0; i < this.array.length; i++) {
-          if (this.array[i] && this.array[i].key)
-          console.log(`${i} - (${this.array[i].hash}) ${this.array[i].key}, ${this.array[i].value}`);
-          else
-          console.log(`${i}`);
-        }
-        console.log(`=============================================`);
-      }
   } // constructor
+
+
+ // ALL PUBLIC - these functions appear in the prototype
+
+  delete(toDeleteKey) {
+    let toDeleteIndex = this.retrieveIndex(toDeleteKey);
+    if (!this.array[toDeleteIndex]) {console.log(`Sorry! this element does not exist`);return false;}
+    let toDeleteHash = this.array[toDeleteIndex].hash;
+    if (this.array[toDeleteIndex]) {
+      this.array[toDeleteIndex] = null;
+      for (let i = toDeleteIndex+1; i < this.array.length; i++ ) {
+        if (!this.array[i]) { console.log(`cell ${i} is empty. We're done`);return true;}
+        else {
+          if (this.array[i].hash <= toDeleteHash) {
+              this.array[toDeleteIndex] = this.array[i];
+              toDeleteIndex = i;
+              toDeleteHash = this.array[i].hash;
+              this.array[i] = null;
+          }
+        }
+      } // for
+      console.log(`Sorry! this element does not exist`);return false;
+    }
+    else {console.log(`Sorry! this element does not exist`);return false;}
+  }
+
+  print() {
+    console.log(`-------- length of array: ${this.array.length} --------`);
+    for (let i = 0; i < this.array.length; i++) {
+      if (this.array[i] && this.array[i].key)
+      console.log(`${i} - (${this.array[i].hash}) ${this.array[i].key}, ${this.array[i].value}`);
+      else
+      console.log(`${i}`);
+    }
+    console.log(`=============================================`);
+  }
+
 } // LinearProbe
 
 
